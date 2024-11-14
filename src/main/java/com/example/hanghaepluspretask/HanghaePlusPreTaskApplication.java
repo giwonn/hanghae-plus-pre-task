03df62c61f722be14b0e21150d3212ba499b3f8e
giwonn <l4279625@gmail.com>
2024-11-15 00:09:04 +0900

    feat: 프로젝트 초기화

---
+++ src/main/java/com/example/hanghaepluspretask/HanghaePlusPreTaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.hanghaepluspretask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HanghaePlusPreTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HanghaePlusPreTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/hanghaepluspretask/HanghaePlusPreTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hanghaepluspretask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HanghaePlusPreTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
